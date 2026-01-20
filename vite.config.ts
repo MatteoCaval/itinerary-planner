29f20afbdc069e48937d3c88ced4a406546d450d
Matteo Cavalluzzo <mcavalluzzo.dev@gmail.com>
2026-01-20 17:06:34 +0000

    ci: setup github actions workflow for github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/itinerary-planner/',
   server: {
     host: true, // Needed for Docker
     port: 5173,
